951b2ce3c03ae8bd77a01309bf3cec341d2070a0
Mannamsken <mannamsken123@gmx.de>
2020-07-21 17:45:20 +0200

    Version 1.2.1

--- src/de/instantskillz/challengeplugin/Commands/InvCommand.java
+++ src/de/instantskillz/challengeplugin/Commands/InvCommand.java
@@ -2,6 +2,8 @@
 //-> Mullemann25 and Mannam01
 package de.instantskillz.challengeplugin.Commands;
 
+import de.instantskillz.challengeplugin.Main.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,20 +21,24 @@ public class InvCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("save")) {
-                    inventorySave.put(player.getName(), player.getInventory().getContents());
-                    player.getInventory().clear();
-                    player.sendMessage("§aServer " + "§8>> " + "§aDein Inventar wurde §6gespeichert§a.");
+                if (player.getWorld() != Bukkit.getWorld("world")) {
+                    if (args[0].equalsIgnoreCase("save")) {
+                        inventorySave.put(player.getName(), player.getInventory().getContents());
+                        player.getInventory().clear();
+                        player.sendMessage("§aServer " + "§8>> " + "§aDein Inventar wurde §6gespeichert§a.");
 
-                } else if (args[0].equalsIgnoreCase("get")) {
-                    if (inventorySave.containsKey(player.getName())) {
-                        player.getInventory().setContents(inventorySave.get(player.getName()));
-                        inventorySave.remove(player.getName());
-                        player.sendMessage("§aServer " + "§8>> " + "§aDein Inventar wurde §6geladen§a.");
+                    } else if (args[0].equalsIgnoreCase("get")) {
+                        if (inventorySave.containsKey(player.getName())) {
+                            player.getInventory().setContents(inventorySave.get(player.getName()));
+                            inventorySave.remove(player.getName());
+                            player.sendMessage("§aServer " + "§8>> " + "§aDein Inventar wurde §6geladen§a.");
+                        } else
+                            player.sendMessage("§aServer " + "§8>> " + "§cDu hast noch kein Inventar gespeichert!");
                     } else
-                        player.sendMessage("§aServer " + "§8>> " + "§cDu hast noch kein Inventar gespeichert!");
-                } else
-                    player.sendMessage("§aServer " + "§8>> " + "§cBitte benutze §6/inv save/get§c!");
+                        player.sendMessage("§aServer " + "§8>> " + "§cBitte benutze §6/inv save/get§c!");
+                } else if (player.getWorld() == Bukkit.getWorld("world")) {
+                    player.sendMessage(Main.getPlugin().PREFIX + "§cDieses Kommando funktioniert hier nicht!");
+                }
             } else
                 player.sendMessage("§aServer " + "§8>> " + "§cBitte benutze §6/inv save/get§c!");
         }
