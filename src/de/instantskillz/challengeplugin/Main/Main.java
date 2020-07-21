c6664150146ebd3d44dcd517c96d9671c513ddbd
Mannamsken <mannamsken123@gmx.de>
2020-07-21 16:43:28 +0200

    Version 1.1

---
+++ src/de/instantskillz/challengeplugin/Main/Main.java
@@ -0,0 +1,13 @@
+//Plugin by: InstantSKillzTv
+//-> Mullemann25 and Mannam01
+package de.instantskillz.challengeplugin.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("PenisPumpe");
+    }
+}
